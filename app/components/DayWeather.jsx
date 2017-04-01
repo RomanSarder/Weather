548c5542697a8850ddff22c2815fc1345f5069c3
Роман <roman.sarder@yandex.ru>
2017-04-01 16:20:31 +0300

    Add hover animation show hidden content

--- app/components/DayWeather.jsx
+++ app/components/DayWeather.jsx
@@ -14,20 +14,43 @@ const DayWeather = React.createClass({
         temperatureMin = temperatureMin < 0 ? Math.round(temperatureMin) : '+' + Math.round(temperatureMin)
         return (
             <div className="column ui segment">
-                <div className="row one column">
-                    <div className="column">
+                <div className="flip">
+                    <div className="front">
                         <div className="row one column">
                             <div className="column">
-                                <span className="day-week">{day}</span>
-                                <span className="date">{date} {month}</span>
+                                <div className="row one column">
+                                    <div className="column">
+                                        <span className="day-week">{day}</span>
+                                        <span className="date">{date} {month}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row one column">
+                                <div className="column">
+                                    <span className="day-icon"><Skycon className="icon-day" color='#99663C' icon={icon === 'partly-cloudy-night' ? 'clear-day' : icon} /></span>
+                                    <span className="temperatureMax-day">{temperatureMax}&nbsp;°</span>
+                                    <span className="temperatureMin-day">{temperatureMin}&nbsp;°</span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="row one column">
-                        <div className="column">
-                            <span className="day-icon"><Skycon className="icon-day" color='#99663C' icon={icon === 'partly-cloudy-night' ? 'clear-day' : icon} /></span>
-                            <span className="temperatureMax-day">{temperatureMax}&nbsp;°</span>
-                            <span className="temperatureMin-day">{temperatureMin}&nbsp;°</span>
+                    <div className="back">
+                        <div className="row one column">
+                            <div className="column">
+                                <div className="row one column">
+                                    <div className="column">
+                                        <span className="day-week">Something hidden here</span>
+                                        <span className="date">We hide</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row one column">
+                                <div className="column">
+                                    <span className="day-icon"><Skycon className="icon-day" color='green' icon={icon === 'partly-cloudy-night' ? 'clear-day' : icon} /></span>
+                                    <span className="temperatureMax-day">{temperatureMax}&nbsp;°</span>
+                                    <span className="temperatureMin-day">{temperatureMin}&nbsp;°</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
